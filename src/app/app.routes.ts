5df9ff68df6d4529961bb23519ac7760a9c11d0b
khalid <khalid.fati7i.hb@gmail.com>
2024-02-19 17:28:09 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path:"products", component : ProductsComponent},
+{path:"customers", component : CustomersComponent}
+
+];
