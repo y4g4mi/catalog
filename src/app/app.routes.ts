025693236a534036150e6221fd1f9ab69509e891
khalid <khalid.fati7i.hb@gmail.com>
2024-02-20 16:56:48 +0100

    adding login rout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { CustomersComponent } from './customers/customers.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path:'',component: LoginComponent},
+{path:'login',component: LoginComponent},
 {path:"products", component : ProductsComponent},
 {path:"customers", component : CustomersComponent}
 
