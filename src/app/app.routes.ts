dc200b96935226d7eb676d1c4dd1764b8b60126a
khalid <khalid.fati7i.hb@gmail.com>
2024-02-20 16:13:25 +0100

    adding loging page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
 import { CustomersComponent } from './customers/customers.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
+    {path:'',component: LoginComponent},
 {path:"products", component : ProductsComponent},
 {path:"customers", component : CustomersComponent}
 
