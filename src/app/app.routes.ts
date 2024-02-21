bae346e93246c70359337546a3e38180a7153bd1
khalid <khalid.fati7i.hb@gmail.com>
2024-02-21 16:24:11 +0100

    adding a template for the product page and fixing the padding in the login form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './products/products.component';
-import { CustomersComponent } from './customers/customers.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+{path:'',component: HomeComponent},
+{path:'home',component: HomeComponent},
 {path:'login',component: LoginComponent},
 {path:"products", component : ProductsComponent},
-{path:"customers", component : CustomersComponent}
-
+{path:"**", component : PageNotFoundComponent}
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
